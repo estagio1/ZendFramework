0f413843e587011be2a50d42114294db4578b881
Estagio 1 <estagio@sistemaonix.com>
2017-06-19 09:24:00 -0300

    remove: removido o arquivo .gitignore de config/autoload

--- module/Album/src/Album/Controller/ControllerAbstract.php
+++ module/Album/src/Album/Controller/ControllerAbstract.php
@@ -65,6 +65,8 @@ abstract class ControllerAbstract extends AbstractActionController{
                 'form' => $form,
                 'success' => $this->flashMessenger()->getErrorMessages()]);
         }
+        
+        $this->flashMessenger()->clearMessages();
         return new ViewModel(['form' => $form]);
     }
     /**
@@ -72,7 +74,55 @@ abstract class ControllerAbstract extends AbstractActionController{
      * @return array|void
      */
     public function editarAction(){
+        if(is_string($this->form)){
+            $form = new $this->form;
+        } else {
+            $form = $this->form;
+        }
+        $request = $this->getRequest();
+        $param = $this->params()->fromRoute('id', 0);
+        $repository = $this->getEm()->getRepository($this->entity)->findOneBy($param);
         
+        if($repository){
+            if($request->isPost()){
+                $form->setData($request->getPost());
+                if($form->isValid()){
+                    $service = $this->getServiceLocator()->get($this->service);
+                    $data = $request->getPost()->toArray();
+                    $data['id'] = $param;
+                    if($service->save($data)){
+                        $this->flashMessenger()->addSucessMessage('Atualizado com sucesso!');
+                    } else {
+                        $this->flashMessenger()->addErrorMessage('Não foi possivel atualizar! Tente mais tarde.');
+                    }
+                    return $this->redirect()->toRoute($this->route, array('controller' => $this->controller));
+                }
+            }
+        } else {
+            $this->flashMessenger()->addInfoMessage('Registro não foi encontrado!');
+            return $this->redirect()->toRoute($this->route, array('controller' => $this->controller));
+        }
+        
+        if($this->flashMessenger()->hasSuccessMensages()){
+            return new ViewModel([
+                'form' => $form,
+                'success' => $this->flashMessenger()->getSuccessMessages(),
+                'id' => $param]);
+        }
+        if($this->flashMessenger()->hasErrorMensages()){
+            return new ViewModel([
+                'form' => $form,
+                'success' => $this->flashMessenger()->getErrorMessages(),
+                'id' => $param]);
+        }
+        if($this->flashMessenger()->hasInfoMensages()){
+            return new ViewModel([
+                'form' => $form,
+                'warning' => $this->flashMessenger()->getInfoMessages(),
+                'id' => $param]);
+        }
+        $this->flashMessenger()->clearMessages();
+        return new ViewModel(['form' => $form]);
     }
     /**
      * Excluir um registro
